0d48904ac550aaaa4989d71d92f0a8062bcdb217
Finance Tracker <dauricejordan@gmail.com>
2018-09-30 21:17:41 -0400

    Add user and stock association plus views

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,10 +2,8 @@ class StocksController < ApplicationController
   
   def search
     @stock = Stock.new_from_lookup(params[:stock])
-    flash.now[:danger] = "Invalid Stock Entered \"" + params[:stock] + "\"" unless @stock
-    #if !@stock
-    #  flash.now[:danger] = "Invalid Stock Entered \"" + params[:stock] + "\"" 
-    #end
+    #flash.now[:danger] = "Invalid Stock Entered \"" + params[:stock] + "\"" unless @stock
+    flash.now[:danger] = "Invalid Stock Entered \" #{params[:stock]}\"" unless @stock
     render partial: 'users/result'
   end
   
