7cf0e074231e1d7b128c0d69cce5d111c8814136
Finance Tracker <dauricejordan@gmail.com>
2018-10-01 16:02:59 -0400

    Add friend tracking

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,10 +1,19 @@
 class StocksController < ApplicationController
   
+  #def search
+  # @stock = Stock.new_from_lookup(params[:stock])
+  #  #flash.now[:danger] = "Invalid Stock Entered \"" + params[:stock] + "\"" unless @stock
+  #  flash.now[:danger] = "Invalid Stock Entered \" #{params[:stock]}\"" unless @stock
+  #  render partial: 'users/result'
+  #end
+  
   def search
-    @stock = Stock.new_from_lookup(params[:stock])
-    #flash.now[:danger] = "Invalid Stock Entered \"" + params[:stock] + "\"" unless @stock
-    flash.now[:danger] = "Invalid Stock Entered \" #{params[:stock]}\"" unless @stock
+    if params[:stock].blank?
+      flash.now[:danger] = "You have entered an empty search string"
+    else
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
+    end
     render partial: 'users/result'
   end
-  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,43 @@ class UsersController < ApplicationController
     end
   
     def my_friends
+      @friendships = current_user.friends
+    end
+  
+    def search
+      #@users = User.search(params[:search_param])
+      #flash.now[:danger] = "Invalid Stock Entered \"" + params[:stock] + "\"" unless @stock
+      #flash.now[:danger] = "No users match this search criteria \"#{params[:search_param]}\"" if @users.blank?
+      #render partial: 'friends/result'
+      if params[:search_param].blank?
+        flash.now[:danger] = "You have entered an empty search string"
+      else
+        @users = User.search(params[:search_param])
+        @users = current_user.except_current_user(@users)
+        #puts @users
+        flash.now[:danger] = "No users match this search criteria" if @users.blank?
+      end
+      render partial: 'friends/result'
+      #@users = User.search(params[:search_param])
+      #render json: @users
       
     end
+  
+    def add_friend
+      @friend = User.find(params[:friend])
+      current_user.friendships.build(friend_id: @friend.id)
+      
+      if current_user.save
+        flash[:success] = "Friend was successfully added"
+      else
+        flash[:danger] = "There was something wrong with the friend request"
+      end
+      redirect_to my_friends_path 
+    end
+  
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks
+  end
 
 end
