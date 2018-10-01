2f5b979e3dfc3c87eb669dffae776f866b801140
Finance Tracker <dauricejordan@gmail.com>
2018-09-30 23:57:46 -0400

    Add Friendship Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  
+  has_many :friendships
+  has_many :friendships, through: :friendships
   
   def full_name
     return "#{first_name} #{last_name}".strip if (first_name || last_name)
